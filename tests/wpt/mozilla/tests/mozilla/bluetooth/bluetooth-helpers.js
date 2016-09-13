3731b75a1a1d6f326bc677abbd92093187a2c4a0
Attila Dusnoki <dati91@gmail.com>
2016-09-13 15:38:15 +0200

    Add WebBluetooth WPT requestDevice tests

---
+++ tests/wpt/mozilla/tests/mozilla/bluetooth/bluetooth-helpers.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// Bluetooth UUID constants:
+// Services:
+var blacklist_test_service_uuid = "611c954a-263b-4f4a-aab6-01ddb953f985";
+var request_disconnection_service_uuid = "01d7d889-7451-419f-aeb8-d65e7b9277af";
+// Characteristics:
+var blacklist_exclude_reads_characteristic_uuid =
+  "bad1c9a2-9a5b-4015-8b60-1579bbbf2135";
+var request_disconnection_characteristic_uuid =
+  "01d7d88a-7451-419f-aeb8-d65e7b9277af";
+
+// Sometimes we need to test that using either the name, alias, or UUID
+// produces the same result. The following objects help us do that.
+var generic_access = {
+  alias: 0x1800,
+  name: 'generic_access',
+  uuid: '00001800-0000-1000-8000-00805f9b34fb'
+};
+var device_name = {
+  alias: 0x2a00,
+  name: 'gap.device_name',
+  uuid: '00002a00-0000-1000-8000-00805f9b34fb'
+};
+var reconnection_address = {
+  alias: 0x2a03,
+  name: 'gap.reconnection_address',
+  uuid: '00002a03-0000-1000-8000-00805f9b34fb'
+};
+var heart_rate = {
+  alias: 0x180d,
+  name: 'heart_rate',
+  uuid: '0000180d-0000-1000-8000-00805f9b34fb'
+};
+var body_sensor_location = {
+  alias: 0x2a38,
+  name: 'body_sensor_location',
+  uuid: '00002a38-0000-1000-8000-00805f9b34fb'
+};
+var glucose = {
+  alias: 0x1808,
+  name: 'glucose',
+  uuid: '00001808-0000-1000-8000-00805f9b34fb'
+};
+var battery_service = {
+  alias: 0x180f,
+  name: 'battery_service',
+  uuid: '0000180f-0000-1000-8000-00805f9b34fb'
+};
+var battery_level = {
+  alias: 0x2A19,
+  name: 'battery_level',
+  uuid: '00002a19-0000-1000-8000-00805f9b34fb'
+};
