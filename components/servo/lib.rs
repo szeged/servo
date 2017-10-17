d9dcc6ac9bf5e33bebbc6923bb8072a9b62b5a3d
Zakor Gyula <zakorgy@inf.u-szeged.hu>
2017-10-17 15:36:08 +0200

    apply latest webrender changes

--- components/servo/lib.rs
+++ components/servo/lib.rs
@@ -102,7 +102,7 @@ use std::cmp::max;
 use std::path::PathBuf;
 use std::rc::Rc;
 use std::sync::mpsc::{Sender, channel};
-use webrender::RendererKind;
+use webrender::{BackendDevice, Factory, RendererKind, RTV, DSV};
 use webvr::{WebVRThread, WebVRCompositorHandler};
 
 pub use gleam::gl;
@@ -128,7 +128,7 @@ pub struct Servo<Window: WindowMethods + 'static> {
 }
 
 impl<Window> Servo<Window> where Window: WindowMethods + 'static {
-    pub fn new(window: Rc<Window>) -> Servo<Window> {
+    pub fn new(window: Rc<Window>, device: BackendDevice, factory:  Factory, main_color: RTV, main_depth: DSV) -> Servo<Window> {
         // Global configuration options, parsed from the command line.
         let opts = opts::get();
 
@@ -157,7 +157,7 @@ impl<Window> Servo<Window> where Window: WindowMethods + 'static {
         let mut resource_path = resources_dir_path().unwrap();
         resource_path.push("shaders");
 
-        let (mut webrender, webrender_api_sender, _) = {
+        let (mut webrender, webrender_api_sender) = {
             // TODO(gw): Duplicates device_pixels_per_screen_px from compositor. Tidy up!
             let scale_factor = window.hidpi_factor().get();
             let device_pixel_ratio = match opts.device_pixels_per_px {
@@ -184,7 +184,6 @@ impl<Window> Servo<Window> where Window: WindowMethods + 'static {
 
             let mut debug_flags = webrender::DebugFlags::empty();
             debug_flags.set(webrender::DebugFlags::PROFILER_DBG, opts.webrender_stats);
-
             let render_notifier = Box::new(RenderNotifier::new(compositor_proxy.clone()));
 
             webrender::Renderer::new(render_notifier, webrender::RendererOptions {
@@ -200,7 +199,7 @@ impl<Window> Servo<Window> where Window: WindowMethods + 'static {
                 renderer_kind: renderer_kind,
                 enable_subpixel_aa: opts.enable_subpixel_text_antialiasing,
                 ..Default::default()
-            }).expect("Unable to initialize webrender!")
+            }, device, factory, main_color, main_depth).expect("Unable to initialize webrender!")
         };
 
         let webrender_api = webrender_api_sender.create_api();
